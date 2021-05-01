cf51083876218cd2d7d82edbbcd730372bc084ff
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2021-05-01 17:49:38 +0530

    setttings updated

--- Agridoc/settings.py
+++ Agridoc/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
