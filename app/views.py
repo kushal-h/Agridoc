ead422ded44d5d8012235001c35dfe571aeca536
KUSHAL H <56520530+kushal-h@users.noreply.github.com>
2021-05-01 17:50:22 +0530

    views uupdated

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 import os
-
+import cv2
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.shortcuts import render
@@ -11,8 +11,9 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image as Image
 import numpy as np
 from app.models import Predictions, ImagePath
-# Create your views here.
 
+# Create your views here.
+from values import data
 
 
 def home(request):
@@ -25,11 +26,9 @@ def predict(request):
             folder = 'media/images/'
             image = request.FILES['cd']
             print('image name is: ',image.name)
-
-            file_name = str(image.name)
             fs = FileSystemStorage(location = folder)
             name = fs.save(image.name, image)
-
+            print(name)
             media_path = folder + "{}"
             file_path = os.path.join(media_path).format(name)
             print('the file path is ',file_path)
@@ -37,28 +36,30 @@ def predict(request):
             #Loading the model
             print('model is loding............')
             print("tf version",tf.__version__)
-            model = tf.keras.models.load_model('model.h5')
+            model = load_model('128_sized_model.h5')
+            print(model.summary())
             print('model is loaded! ')
-            print('model summary is', model.summary())
-            image = Image.load_img(file_path, target_size= (227,227))
+            image = Image.load_img(file_path, target_size=(128, 128))
             image = Image.img_to_array(image)
-            # print(image)
-            image = np.expand_dims(image, axis = 0)
-            # print(image)
+            image = np.expand_dims(image, axis=0)
 
+
+            # messages.success(request, 'working fine')
 
             #Prediction
-            r = np.argmax(model.predict(image))
-            result = model.predict(image)
+            result = model.predict([image])
+            print(result)
+            r = np.argmax(result, axis=1)
             print("the results are!",r, type(r))
-            if r == 7:
-
+            #
+            pred =data[r[0]]
+            print(pred)
 
             img_path = ImagePath()
             img_path.path = file_path
             img_db = Predictions()
             img_db.image_path = file_path
-            #img_db.prediction= pred
+            img_db.prediction= pred
             img_db.save()
             return render(request, "results.html", {'image_source': img_path})
 
